d3fcd3f10f147e53e8c5cdb005c3322cf4093cc9
Samuel O. Asare <asare11samuel@gmail.com>
2022-10-19 23:24:51 +0000

    math functions

---
+++ withMosh/mathFunctions.py
@@ -0,0 +1,16 @@
+import math
+x = 2.9
+
+# round numbers
+newValue = round(x)
+
+# absolute value
+absValue = abs(-x)
+
+# math.ceil
+ceil = math.ceil(x)
+
+# math.floor
+floor = math.floor(x)
+
+print(newValue, absValue, floor)
