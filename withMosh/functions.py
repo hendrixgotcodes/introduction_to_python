5821a44b2c866ae8496c756a1bd928c3196c19f7
Samuel O. Asare <asare11samuel@gmail.com>
2022-10-27 15:32:39 +0000

    functions: return statement

--- withMosh/functions.py
+++ withMosh/functions.py
@@ -1,7 +1,4 @@
-def greet_user(first_name, last_name):
-    print(f"Hi {first_name} {last_name}")
-    print("Welcome aboard")
+def square(number):
+    return number*number
 
-greet_user("Peter", "Griffin")
-
-greet_user(last_name="Griffin", first_name="Stewie")
+print(square(16))
